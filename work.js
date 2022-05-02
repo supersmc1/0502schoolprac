db581eaa5e12382d9de9cd6ba31e69a12bfee535
sunwanchung <sunwanchung@gmail.com>
2022-05-02 17:57:46 +0900

    공부

---
+++ work.js
@@ -0,0 +1,41 @@
+//console.log('test');
+//let one;// todo : 변수 선언은 했고 값은 무엇을 가리키지는 않은 상태
+//1;
+// todo : 식별을 위해서 값과 변수를 '대입'하는 과정을 통한다 = (이퀄표시)
+
+//let test = 1; //? 변수 선언과 동시에 값을 가리켰다. 초기화되었다.
+//? RAM 이라는 메모리 일단 담긴다 "식별할 방법" = 변수의 큰의미
+//값은 존재하는 상태
+//활용이 아직 안된상태
+//변수를 왜쓰는가
+//데이터 숫자타입 1이라는 저 값을 쓸방법이 없음 ->변수를 사용하지 않으면
+//console.log(one);
+//const two = 1; // ? 변수 선언과 동시에 값도 같이 가리키게 만든다 . const 의 규칙
+// 에러가 났다 === 해결만 한다면 무결해짐
+
+//console.log(two);
+//console.log(1 === "1");
+
+//let a = 1;
+//let b = "진";
+
+//let change = String(b);
+
+
+//console.log(typeof change);
+//console.log( a === b );
+//console.log(typeof a === typeof b );
+
+//let chea = null;
+// todo : null 존재는 하지만 비어있는값, 프로그래밍에만 있는 개념 "빈공간"
+// todo : null 이라는 값이 존재하면 : 아직 값이 정해지지 않았다.
+
+ //console.log(typeof chea);
+
+ //let arr = ["피카츄", "라이츄", "파이리", "꼬부기"];
+// console.log(typeof arr)
+ //console.log(Array.isArray(arr));//배열 검출
+
+ let text = "나는 잘생긴 공욱재다";
+ console.log(text.length);
+ console.log(text[7], text[8], text[9]);
